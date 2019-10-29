bb5a527a58e1d01183b545be38be669d09f7a31f
root <root@CloudEdge1.localhost>
2019-10-29 18:37:12 +0800

    add bluebooth device service

--- hack/edgex-down.sh
+++ hack/edgex-down.sh
@@ -6,49 +6,55 @@ EDGEX_ROOT=$(dirname "${BASH_SOURCE}")/..
 
 function delete_services {
 
-  kubectl delete -f "${EDGEX_ROOT}/services/consul-service.yaml"
-  kubectl delete -f "${EDGEX_ROOT}/services/rulesengine-service.yaml"
-  kubectl delete -f "${EDGEX_ROOT}/services/export-distro-service.yaml"
-  kubectl delete -f "${EDGEX_ROOT}/services/export-client-service.yaml"
-  kubectl delete -f "${EDGEX_ROOT}/services/scheduler-service.yaml"
-  kubectl delete -f "${EDGEX_ROOT}/services/command-service.yaml"
-  kubectl delete -f "${EDGEX_ROOT}/services/data-service.yaml"
-  kubectl delete -f "${EDGEX_ROOT}/services/metadata-service.yaml"
-  kubectl delete -f "${EDGEX_ROOT}/services/notifications-service.yaml"
-  kubectl delete -f "${EDGEX_ROOT}/services/logging-service.yaml"
-  kubectl delete -f "${EDGEX_ROOT}/services/mongo-service.yaml"
-  kubectl delete -f "${EDGEX_ROOT}/services/ui-edgex-service.yaml"
+  rancher kubectl delete -f "${EDGEX_ROOT}/services/consul-service.yaml"
+  rancher kubectl delete -f "${EDGEX_ROOT}/services/vault-service.yaml"
+  rancher kubectl delete -f "${EDGEX_ROOT}/services/rulesengine-service.yaml"
+  rancher kubectl delete -f "${EDGEX_ROOT}/services/export-distro-service.yaml"
+  rancher kubectl delete -f "${EDGEX_ROOT}/services/export-client-service.yaml"
+  rancher kubectl delete -f "${EDGEX_ROOT}/services/scheduler-service.yaml"
+  rancher kubectl delete -f "${EDGEX_ROOT}/services/command-service.yaml"
+  rancher kubectl delete -f "${EDGEX_ROOT}/services/data-service.yaml"
+  rancher kubectl delete -f "${EDGEX_ROOT}/services/metadata-service.yaml"
+  rancher kubectl delete -f "${EDGEX_ROOT}/services/notifications-service.yaml"
+  rancher kubectl delete -f "${EDGEX_ROOT}/services/logging-service.yaml"
+  rancher kubectl delete -f "${EDGEX_ROOT}/services/mongo-service.yaml"
+  rancher kubectl delete -f "${EDGEX_ROOT}/services/ui-edgex-service.yaml"
+  rancher kubectl delete -f "${EDGEX_ROOT}/services/modbus-service.yaml"
 }
 
 function delete_deployments {
 
-  kubectl delete -f "${EDGEX_ROOT}/deployments/volume-deployment.yaml"
+  rancher kubectl delete -f "${EDGEX_ROOT}/deployments/volume-deployment.yaml"
   sleep 10
-  kubectl delete -f "${EDGEX_ROOT}/deployments/consul-deployment.yaml"
+  rancher kubectl delete -f "${EDGEX_ROOT}/deployments/consul-deployment.yaml"
   sleep 10
-  kubectl delete -f "${EDGEX_ROOT}/deployments//config-seed-deployment.yaml"
+  rancher kubectl delete -f "${EDGEX_ROOT}/deployments/config-seed-job.yaml"
   sleep 10
-  kubectl delete -f "${EDGEX_ROOT}/deployments/rulesengine-deployment.yaml"
+  rancher kubectl delete -f "${EDGEX_ROOT}/deployments/vault-deployment.yaml"
   sleep 10
-  kubectl delete -f "${EDGEX_ROOT}/deployments/export-distro-deployment.yaml"
+  rancher kubectl delete -f "${EDGEX_ROOT}/deployments/rulesengine-deployment.yaml"
   sleep 10
-  kubectl delete -f "${EDGEX_ROOT}/deployments/export-client-deployment.yaml"
+  rancher kubectl delete -f "${EDGEX_ROOT}/deployments/export-distro-deployment.yaml"
   sleep 10
-  kubectl delete -f "${EDGEX_ROOT}/deployments/scheduler-deployment.yaml"
+  rancher kubectl delete -f "${EDGEX_ROOT}/deployments/export-client-deployment.yaml"
   sleep 10
-  kubectl delete -f "${EDGEX_ROOT}/deployments/command-deployment.yaml"
+  rancher kubectl delete -f "${EDGEX_ROOT}/deployments/scheduler-deployment.yaml"
   sleep 10
-  kubectl delete -f "${EDGEX_ROOT}/deployments/data-deployment.yaml"
+  rancher kubectl delete -f "${EDGEX_ROOT}/deployments/command-deployment.yaml"
   sleep 10
-  kubectl delete -f "${EDGEX_ROOT}/deployments/metadata-deployment.yaml"
+  rancher kubectl delete -f "${EDGEX_ROOT}/deployments/data-deployment.yaml"
   sleep 10
-  kubectl delete -f "${EDGEX_ROOT}/deployments/notifications-deployment.yaml"
+  rancher kubectl delete -f "${EDGEX_ROOT}/deployments/metadata-deployment.yaml"
   sleep 10
-  kubectl delete -f "${EDGEX_ROOT}/deployments/logging-deployment.yaml"
+  rancher kubectl delete -f "${EDGEX_ROOT}/deployments/notifications-deployment.yaml"
   sleep 10
-  kubectl delete -f "${EDGEX_ROOT}/deployments/mongo-deployment.yaml"
+  rancher kubectl delete -f "${EDGEX_ROOT}/deployments/logging-deployment.yaml"
   sleep 10
-  kubectl delete -f "${EDGEX_ROOT}/deployments/ui-deployment.yaml"
+  rancher kubectl delete -f "${EDGEX_ROOT}/deployments/mongo-deployment.yaml"
+  sleep 10
+  rancher kubectl delete -f "${EDGEX_ROOT}/deployments/ui-deployment.yaml"
+  sleep 10
+  rancher kubectl delete -f "${EDGEX_ROOT}/deployments/device-modbus-deployment.yaml"
   sleep 10
 }
 
--- hack/edgex-up.sh
+++ hack/edgex-up.sh
@@ -6,49 +6,56 @@ EDGEX_ROOT=$(dirname "${BASH_SOURCE}")/..
 
 function create_services {
 
-  kubectl create -f "${EDGEX_ROOT}/services/consul-service.yaml"
-  kubectl create -f "${EDGEX_ROOT}/services/mongo-service.yaml"
-  kubectl create -f "${EDGEX_ROOT}/services/logging-service.yaml"
-  kubectl create -f "${EDGEX_ROOT}/services/notifications-service.yaml"
-  kubectl create -f "${EDGEX_ROOT}/services/metadata-service.yaml"
-  kubectl create -f "${EDGEX_ROOT}/services/data-service.yaml"
-  kubectl create -f "${EDGEX_ROOT}/services/command-service.yaml"
-  kubectl create -f "${EDGEX_ROOT}/services/scheduler-service.yaml"
-  kubectl create -f "${EDGEX_ROOT}/services/export-client-service.yaml"
-  kubectl create -f "${EDGEX_ROOT}/services/export-distro-service.yaml"
-  kubectl create -f "${EDGEX_ROOT}/services/rulesengine-service.yaml"
-  kubectl create -f "${EDGEX_ROOT}/services/ui-edgex-service.yaml"
+  rancher kubectl create -f "${EDGEX_ROOT}/services/consul-service.yaml"
+  rancher kubectl create -f "${EDGEX_ROOT}/services/vault-service.yaml"
+  rancher kubectl create -f "${EDGEX_ROOT}/services/mongo-service.yaml"
+  rancher kubectl create -f "${EDGEX_ROOT}/services/logging-service.yaml"
+  rancher kubectl create -f "${EDGEX_ROOT}/services/notifications-service.yaml"
+  rancher kubectl create -f "${EDGEX_ROOT}/services/metadata-service.yaml"
+  rancher kubectl create -f "${EDGEX_ROOT}/services/data-service.yaml"
+  rancher kubectl create -f "${EDGEX_ROOT}/services/command-service.yaml"
+  rancher kubectl create -f "${EDGEX_ROOT}/services/scheduler-service.yaml"
+  rancher kubectl create -f "${EDGEX_ROOT}/services/export-client-service.yaml"
+  rancher kubectl create -f "${EDGEX_ROOT}/services/export-distro-service.yaml"
+  rancher kubectl create -f "${EDGEX_ROOT}/services/rulesengine-service.yaml"
+  rancher kubectl create -f "${EDGEX_ROOT}/services/ui-edgex-service.yaml"
+  rancher kubectl create -f "${EDGEX_ROOT}/services/modbus-service.yaml"
 }
 
 function create_deployments {
 
-  kubectl create -f "${EDGEX_ROOT}/deployments/volume-deployment.yaml"
+  rancher kubectl create -f "${EDGEX_ROOT}/deployments/volume-deployment.yaml"
   sleep 10
-  kubectl create -f "${EDGEX_ROOT}/deployments/consul-deployment.yaml"
+  rancher kubectl create -f "${EDGEX_ROOT}/deployments/consul-deployment.yaml"
   sleep 10
-  kubectl create -f "${EDGEX_ROOT}/deployments/config-seed-deployment.yaml"
+  rancher kubectl create -f "${EDGEX_ROOT}/deployments/config-seed-job.yaml"
   sleep 10
-  kubectl create -f "${EDGEX_ROOT}/deployments/mongo-deployment.yaml"
+  rancher kubectl create -f "${EDGEX_ROOT}/deployments/vault-deployment.yaml"
   sleep 10
-  kubectl create -f "${EDGEX_ROOT}/deployments/logging-deployment.yaml"
+  rancher kubectl create -f "${EDGEX_ROOT}/deployments/mongo-deployment.yaml"
   sleep 10
-  kubectl create -f "${EDGEX_ROOT}/deployments/notifications-deployment.yaml"
+  rancher kubectl create -f "${EDGEX_ROOT}/deployments/logging-deployment.yaml"
   sleep 10
-  kubectl create -f "${EDGEX_ROOT}/deployments/metadata-deployment.yaml"
+  rancher kubectl create -f "${EDGEX_ROOT}/deployments/notifications-deployment.yaml"
   sleep 10
-  kubectl create -f "${EDGEX_ROOT}/deployments/data-deployment.yaml"
+  rancher kubectl create -f "${EDGEX_ROOT}/deployments/metadata-deployment.yaml"
   sleep 10
-  kubectl create -f "${EDGEX_ROOT}/deployments/command-deployment.yaml"
+  rancher kubectl create -f "${EDGEX_ROOT}/deployments/data-deployment.yaml"
   sleep 10
-  kubectl create -f "${EDGEX_ROOT}/deployments/scheduler-deployment.yaml"
+  rancher kubectl create -f "${EDGEX_ROOT}/deployments/command-deployment.yaml"
   sleep 10
-  kubectl create -f "${EDGEX_ROOT}/deployments/export-client-deployment.yaml"
+  rancher kubectl create -f "${EDGEX_ROOT}/deployments/scheduler-deployment.yaml"
   sleep 10
-  kubectl create -f "${EDGEX_ROOT}/deployments/export-distro-deployment.yaml"
+  rancher kubectl create -f "${EDGEX_ROOT}/deployments/export-client-deployment.yaml"
   sleep 10
-  kubectl create -f "${EDGEX_ROOT}/deployments/rulesengine-deployment.yaml"
+  rancher kubectl create -f "${EDGEX_ROOT}/deployments/export-distro-deployment.yaml"
   sleep 10
-  kubectl create -f "${EDGEX_ROOT}/deployments/ui-deployment.yaml"
+  rancher kubectl create -f "${EDGEX_ROOT}/deployments/rulesengine-deployment.yaml"
+  sleep 10
+  rancher kubectl create -f "${EDGEX_ROOT}/deployments/ui-deployment.yaml"
+  sleep 10
+  rancher kubectl create -f "${EDGEX_ROOT}/deployments/device-modbus-deployment.yaml"
+  sleep 10
 }
 
 echo "Creating EdgeX services now!"
